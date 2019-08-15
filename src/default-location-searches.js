68d73745b02d70517226fbd774667a3e8b669c3e
Mohamedk93 <50115309+Mohamedk93@users.noreply.github.com>
2019-08-15 16:18:01 +0400

    Update default-location-searches.js

--- src/default-location-searches.js
+++ src/default-location-searches.js
@@ -7,33 +7,3 @@ const { LatLng, LatLngBounds } = sdkTypes;
 //
 // Each item in the array should be an object with a unique `id` (String) and a
 // `predictionPlace` (util.types.place) properties.
-export default [
-  {
-    id: 'default-bayarea',
-    predictionPlace: {
-      address: 'Bay Area',
-      bounds: new LatLngBounds(new LatLng(38.8642448, -121.20817799999998), new LatLng(36.8941549, -123.632497)),
-    },
-  },
-  {
-    id: 'default-financialdistrict',
-    predictionPlace: {
-      address: 'Financial District',
-      bounds: new LatLngBounds(new LatLng(37.798916, -122.39513650000004), new LatLng(37.7866303, -122.40704790000001)),
-    },
-  },
-  {
-    id: 'default-southsf',
-    predictionPlace: {
-      address: 'South SF',
-      bounds: new LatLngBounds(new LatLng(37.6728499, -122.22053110000002), new LatLng(37.6324597, -122.47168399999998)),
-    },
-  },
-  {
-    id: 'default-allsf',
-    predictionPlace: {
-      address: 'All San Francisco',
-      bounds: new LatLngBounds(new LatLng(37.812, -122.34820000000002), new LatLng(37.70339999999999, -122.52699999999999)),
-    },
-  },
-];
