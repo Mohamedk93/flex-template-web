42c6fa1d433c99f500b28aef958d72779d90d587
Mohamed K <mohamed.k.mesbah@gmail.com>
2020-05-21 01:31:57 +0400

    Revert "Update config.js"
    This reverts commit 1bbbd42c209409bae6d00a132149a05b9b2e1cdb.

--- src/config.js
+++ src/config.js
@@ -148,7 +148,7 @@ const maps = {
     // the original coordinates. The actual value is random, but the
     // obfuscated coordinates are withing a circle that has the same
     // radius as the offset.
-    offset: 10000,
+    offset: 500,
 
     // Default zoom level when showing a single circle on a Map. Should
     // be small enough so the whole circle fits in.
