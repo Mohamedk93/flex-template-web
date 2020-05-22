9341abb74b6c747cd702439180a5fa1c65817605
Joumana Elshiekh <joumana.elshiekh@hotdesk-app.com>
2020-05-22 15:10:27 +0100

    Update searchbox.js
    fixing error

--- src/components/SearchBox/searchbox.js
+++ src/components/SearchBox/searchbox.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-import css from './searchbox.css';
+import css from './SearchBox.css';
 
 export class SearchBox extends Component {
     render () {
