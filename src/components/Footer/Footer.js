eaa67ee1d147e6de250c36c63a296e65f9cf7239
Joumana Elshiekh <joumana.elshiekh@hotdesk-app.com>
2020-06-04 11:14:10 +0100

    Revert "Revert "adding frequently asked questions""
    This reverts commit dd2937eab55b0ad80ecf64b89d13dcfec37ab32d.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -93,7 +93,7 @@ const Footer = props => {
                   </NamedLink>
                 </li>
                 <li className={css.listItem}>
-                  <NamedLink name="LandingPage" className={css.link}>
+                  <NamedLink name="FrequentlyAskedQuestions" to={'../FrequentlyAskedQuestions'} className={css.link}>
                     <FormattedMessage id="Footer.toFAQPage" />
                   </NamedLink>
                 </li>
