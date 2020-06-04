3a2b243b9a78176702c7d8b20439f99a7258caf8
Joumana Elshiekh <joumana.elshiekh@hotdesk-app.com>
2020-06-04 19:42:22 +0100

    making seperate dropdown file

--- src/containers/FrequentlyAskedQuestions/FrequentlyAskedQuestions.js
+++ src/containers/FrequentlyAskedQuestions/FrequentlyAskedQuestions.js
@@ -1,73 +1,18 @@
-import React, { useState } from 'react';
-import onClickOutside from 'react-onclickoutside';
+import React from 'react';
 import './FrequentlyAskedQuestions.scss';
-import { StaticPage, TopbarContainer } from '..';
-import {
-    LayoutSingleColumn,
-    LayoutWrapperTopbar,
-    LayoutWrapperMain,
-    LayoutWrapperFooter,
-    Footer,
-  } from '../../components';
+import Dropdown from './Dropdown';
 
-  const FrequentlyAskedQuestions = () =>  {
-  const [open, setOpen] = useState(false);
-  const toggle = () => setOpen(!open);
-  FrequentlyAskedQuestions.handleClickOutside = () => setOpen(false);
 
-  
- 
+
+function FrequentlyAskedQuestions() {
   return (
-    <StaticPage
-    title="FAQs"
-    schema={{
-      '@context': 'http://schema.org',
-      '@type': 'FAQs',
-      description: 'Frequently asked questions',
-      name: 'Frequently asked questions',
-    }}
-  >
-      <LayoutSingleColumn>
-        <LayoutWrapperTopbar>
-          <TopbarContainer />
-        </LayoutWrapperTopbar>
-
-        <LayoutWrapperMain className="dd-wrapper">
-      
-        <div
-        tabIndex={0}
-        className="dd-header"
-        role="button"
-        onKeyPress={() => toggle(!open)}
-        onClick={() => toggle(!open)}
-           >
-        <div className="dd-header__title">
-          <p className="dd-header__title--bold">What is Hotdesk?</p>
-        </div>
-        <div className="dd-header__action">
-          <p>{open ? 'Close' : 'Open'}</p>
-        </div>
-      </div>
-      {open && (
-        <p className="text">
-          Studiotime is the largest and most trusted online community to book music studios. We have music studios in 35+ countries on our site. We’re a bootstrap marketplace and have successfully generated thousands of bookings in the past two years since we first started. <br />
-Our mission is to help artists further their careers by making home studios to top-line studios all around the world accessible. We are also dedicated to helping studios share the incredible stories of music that originate in them, generate more bookings, and take their studio business to the world’s most creative and talented artists that search for studios on Studiotime.
-        </p>
-      )}
-   
-    </LayoutWrapperMain>
-
-<LayoutWrapperFooter>
-  <Footer />
-</LayoutWrapperFooter>
-</LayoutSingleColumn>
-
-</StaticPage>
+    <div className="container">
+      <h1 style={{ textAlign: 'center' }}>
+        Frequently asked questions{' '}
+      </h1>
+      <Dropdown title="Select movie" items={items} multiSelect />
+    </div>
   );
 }
 
-const clickOutsideConfig = {
-  handleClickOutside: () => FrequentlyAskedQuestions.handleClickOutside,
-};
-
-export default onClickOutside(FrequentlyAskedQuestions, clickOutsideConfig);
+export default FrequentlyAskedQuestions;
