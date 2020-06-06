64f6d7c1fd76d039a708699de0c1e492e780f285
Joumana Elshiekh <joumana.elshiekh@hotdesk-app.com>
2020-06-06 19:50:13 +0100

    Revert "adding more test questions"
    This reverts commit 2001517197928bcff484ee87b96a42f72eebddfb.

--- src/containers/FrequentlyAskedQuestions/FrequentlyAskedQuestions.js
+++ src/containers/FrequentlyAskedQuestions/FrequentlyAskedQuestions.js
@@ -35,18 +35,6 @@ function FrequentlyAskedQuestions() {
       <Dropdown title="What is Hotdesk?" items="Studiotime is the largest and most trusted online community to book music studios. We have music studios in 35+ countries on our site. We’re a bootstrap marketplace and have successfully generated thousands of bookings in the past two years since we first started. 
 
 Our mission is to help artists further their careers by making home studios to top-line studios all around the world accessible. We are also dedicated to helping studios share the incredible stories of music that originate in them, generate more bookings, and take their studio business to the world’s most creative and talented artists that search for studios on Studiotime." />
-
-      <Dropdown title="How does Hotdesk work?" items="Studiotime is the largest and most trusted online community to book music studios. We have music studios in 35+ countries on our site. We’re a bootstrap marketplace and have successfully generated thousands of bookings in the past two years since we first started. 
-
-Our mission is to help artists further their careers by making home studios to top-line studios all around the world accessible. We are also dedicated to helping studios share the incredible stories of music that originate in them, generate more bookings, and take their studio business to the world’s most creative and talented artists that search for studios on Studiotime." />
-    
-    <Dropdown title="What does Hotdesk do?" items="Studiotime is the largest and most trusted online community to book music studios. We have music studios in 35+ countries on our site. We’re a bootstrap marketplace and have successfully generated thousands of bookings in the past two years since we first started. 
-
-Our mission is to help artists further their careers by making home studios to top-line studios all around the world accessible. We are also dedicated to helping studios share the incredible stories of music that originate in them, generate more bookings, and take their studio business to the world’s most creative and talented artists that search for studios on Studiotime." />
-   <Dropdown title="Why should I use Hotdesk?" items="Studiotime is the largest and most trusted online community to book music studios. We have music studios in 35+ countries on our site. We’re a bootstrap marketplace and have successfully generated thousands of bookings in the past two years since we first started. 
-
-Our mission is to help artists further their careers by making home studios to top-line studios all around the world accessible. We are also dedicated to helping studios share the incredible stories of music that originate in them, generate more bookings, and take their studio business to the world’s most creative and talented artists that search for studios on Studiotime." />
-   
     </div>
     </LayoutWrapperMain>
 
