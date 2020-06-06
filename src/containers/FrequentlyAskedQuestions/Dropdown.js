1d950303669e2b27da2f24b53ceb52b9c6042541
Joumana Elshiekh <joumana.elshiekh@hotdesk-app.com>
2020-06-06 17:56:01 +0100

    testing arrows

--- src/containers/FrequentlyAskedQuestions/Dropdown.js
+++ src/containers/FrequentlyAskedQuestions/Dropdown.js
@@ -20,7 +20,7 @@ function Dropdown({ title, items, multiSelect = false }) {
         onClick={() => toggle(!open)}
            >
           <p className={css.ddheader}>{title}</p>
-          <div>{open ? <p className={css.up}></p>: <p className={css.down}></p>}</div>
+          <div>{open ? <p className={css.arrowup}></p>: <p className={css.arrowdown}></p>}</div>
       {open && (
         <p className={css.text}>
          {items}
