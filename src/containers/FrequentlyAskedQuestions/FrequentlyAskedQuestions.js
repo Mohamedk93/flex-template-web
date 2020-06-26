0fdc0c63c2f9bd77e31e0244207dc009d3740fc0
Mohamed K <mohamed.k.mesbah@gmail.com>
2020-06-26 20:38:26 +0400

    FAQ updates

--- src/containers/FrequentlyAskedQuestions/FrequentlyAskedQuestions.js
+++ src/containers/FrequentlyAskedQuestions/FrequentlyAskedQuestions.js
@@ -32,15 +32,29 @@ function FrequentlyAskedQuestions() {
 
     <div className={css.container}>
       <h1 className={css.pageTitle}>Frequently asked questions</h1>
-      <Dropdown title="What is Hotdesk?" items="Studiotime is the largest and most trusted online community to book music studios. We have music studios in 35+ countries on our site. We’re a bootstrap marketplace and have successfully generated thousands of bookings in the past two years since we first started. 
+      <h1 className={css.pagesubTitle}>Questions for users that want to book workspaces:</h1>
+      <Dropdown title="What is Hotdesk?" items="Hotdesk is the online platform for workspaces. We enable users to book coworking spaces and shared offices worldwide - in 3 clicks. We also enable owners of coworking spaces and shared offices to add their workspaces to be rented by users and make extra income." />
 
-Our mission is to help artists further their careers by making home studios to top-line studios all around the world accessible. We are also dedicated to helping studios share the incredible stories of music that originate in them, generate more bookings, and take their studio business to the world’s most creative and talented artists that search for studios on Studiotime." />
-    <Dropdown title="What is Hotdesk?" items="Studiotime is the largest and most trusted online community to book music studios. We have music studios in 35+ countries on our site. We’re a bootstrap marketplace and have successfully generated thousands of bookings in the past two years since we first started. 
+      <Dropdown title="How does Hotdesk work?" items="Users can easily search and flick through our listings and send a booking request in 3 clicks, after having the option of comparing prices, checking images and knowing the amenities offered by the workspace and send a booking request for an hourly, daily or monthly rate. Users can make a search based on their location, such as checking the nearest workspace or in another country." />
 
-Our mission is to help artists further their careers by making home studios to top-line studios all around the world accessible. We are also dedicated to helping studios share the incredible stories of music that originate in them, generate more bookings, and take their studio business to the world’s most creative and talented artists that search for studios on Studiotime." />
-  
+      <Dropdown title="What types of workspaces are available on Hotdesk?" items="Hotdesk enables you to book coworking spaces and shared offices. Within these categories, you can choose (i) Hotdesks, (ii) Meeting Rooms, or (iii) Private Offices." />
+
+      <Dropdown title="What are the booking plan durations?" items="You can book workspaces for both the short and long term on Hotdesk, whatever suits you. Plans available are (i) Hourly, (ii) Daily and (iii) Monthly." />
+
+      <Dropdown title="Do I pay extra fees to Hotdesk for booking a workspace?" items="No, not at all. Your payment is exactly what you would pay to regularly book the workspace itself. We do not charge extra fees to our customers, as we make our income directly from the Hosts." />
+
+      <h1 className={css.pagesubTitle}>Questions for coworking spaces and shared offices owners that want to become Hosts</h1>
+
+      <Dropdown title="How do I add my space? How long does it take?" items="Adding your coworking space or shared offices usually takes 3 - 5 minutes. All you have to do is go to the + Add your workspace section in the main menu, and follow the required steps. You provide information such as the name and location of the workspace, available workspace types and booking plans, amenities and photos - then you're good to go!" />
+
+      <Dropdown title="What are the benefits of adding my workspace to Hotdesk?" items="Adding your workspace to Hotdesk will enable our customers to view and book your listing. It also gives your workspace a digital experience such as enabiling online bookings and payments, as well as automated invoicing." />
+
+      <Dropdown title="Is it free to add my workspace?" items="Yes, it is 100% free to add your coworking space or shared office to Hotdesk's platform. We only earn a 10% commission upon a successful, paid transaction from our customers." />
+
+      <Dropdown title="How do I receive my online payments? What about cash transactions?" items="Customers can either pay directly at your workspace, or online via their credit cards. For credit card transactions, you will receive your money directly to your bank account, minus our 10% commission and bank fees. You have to enable credit card payments from your payout settings. For cash payments, we will arrange with you for the collection of our commission." />
+
     </div>
-    
+
     </LayoutWrapperMain>
 
 <LayoutWrapperFooter>
