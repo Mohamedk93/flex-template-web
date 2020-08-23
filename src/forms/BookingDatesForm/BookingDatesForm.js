dbdddb43d0cb4dbee9ea5abde634ffbb438341c3
tanawys <amraeltanawy@gmail.com>
2020-08-23 16:21:38 +0200

    add default payment to cash if no stripe config

--- src/forms/BookingDatesForm/BookingDatesForm.js
+++ src/forms/BookingDatesForm/BookingDatesForm.js
@@ -1022,7 +1022,7 @@ export class BookingDatesFormComponent extends Component {
 
 
 
-              <FieldSelect initialValue={stripeConnected?"credit card":undefined} className={css.paymentMethod} id="paymentMethod" name="paymentMethod" label="Choose payment method" validate={requiredSelect}>
+              <FieldSelect initialValue={stripeConnected?"credit card":"cash"} className={css.paymentMethod} id="paymentMethod" name="paymentMethod" label="Choose payment method" validate={requiredSelect}>
                 <option value="">Select payment</option>
                 {stripeConnected ? (<option value="credit card">Credit card</option>) : null}
                 <option value="cash">Cash</option>
